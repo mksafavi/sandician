9d1e8a63198e609a7c8f1dee48059b994153cef1
mksafavi <mksafavi@gmail.com>
2025-08-21 13:07:11 +0330

    removed dup tests

--- src/component/particle.rs
+++ src/component/particle.rs
@@ -229,38 +229,6 @@ impl Grid {
 }
 
 #[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_particle_entity_has_position() {
-        let p = Particle {
-            position: Position { x: 10, y: 20 },
-            particle_type: ParticleType::Sand,
-        };
-        assert_eq!((10, 20), (p.position.x, p.position.y));
-    }
-
-    #[test]
-    fn test_particle_entity_can_have_particle_type_of_sand() {
-        let p = Particle {
-            position: Position { x: 10, y: 20 },
-            particle_type: ParticleType::Sand,
-        };
-        assert_eq!(ParticleType::Sand, p.particle_type);
-    }
-
-    #[test]
-    fn test_particle_entity_can_have_particle_type_of_water() {
-        let p = Particle {
-            position: Position { x: 10, y: 20 },
-            particle_type: ParticleType::Water,
-        };
-        assert_eq!(ParticleType::Water, p.particle_type);
-    }
-}
-
-#[cfg(test)]
 mod tests_grid {
 
     use super::*;
