c5974e2bb42fe6e82a5a2ec748eada9320b26ce0
mksafavi <mksafavi@gmail.com>
2025-08-27 00:00:33 +0330

    added benchmark for update_grid function

---
+++ benches/bench.rs
@@ -0,0 +1,22 @@
+use criterion::{criterion_group, criterion_main, Criterion};
+use sandsim::component::{particle::Grid, particle::Particle};
+
+fn criterion_benchmark(c: &mut Criterion) {
+    c.bench_function("update grid", |b| {
+        let y = 1920;
+        let x = 1080;
+        let mut g = Grid::new(x, y);
+        (0..x)
+            .zip(0..y / 2)
+            .for_each(|(x, y)| g.spawn_particle(x, y, Particle::Sand));
+        (0..x)
+            .zip(y / 2..y)
+            .for_each(|(x, y)| g.spawn_particle(x, y, Particle::Water));
+        b.iter(|| {
+            g.update_grid();
+        });
+    });
+}
+
+criterion_group!(benches, criterion_benchmark);
+criterion_main!(benches);
