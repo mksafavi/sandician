955fb6a23d4f4bc96122195c51dba8b190b6dd5d
mksafavi <mksafavi@gmail.com>
2025-08-27 00:57:27 +0330

    added benchmark for draw_grid

--- benches/bench.rs
+++ benches/bench.rs
@@ -16,7 +16,24 @@ fn criterion_benchmark(c: &mut Criterion) {
             g.update_grid();
         });
     });
+
+    c.bench_function("draw grid", |b| {
+        let y = 1920;
+        let x = 1080;
+        let mut g = Grid::new(x, y);
+        let mut image = Grid::create_output_frame(x, y);
+        (0..x)
+            .zip(0..y / 2)
+            .for_each(|(x, y)| g.spawn_particle(x, y, Particle::Sand));
+        (0..x)
+            .zip(y / 2..y)
+            .for_each(|(x, y)| g.spawn_particle(x, y, Particle::Water));
+        b.iter(|| {
+            g.draw_grid(&mut image);
+        });
+    });
 }
 
 criterion_group!(benches, criterion_benchmark);
 criterion_main!(benches);
+
