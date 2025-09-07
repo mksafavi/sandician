bcc4a0db28a568042a1aa7c20a14861af1db6b5f
mksafavi <mksafavi@gmail.com>
2025-09-07 16:56:11 +0330

    fix benchmark grid fill function
    it should fill the whole grid

--- benches/bench.rs
+++ benches/bench.rs
@@ -1,17 +1,72 @@
 use criterion::{Criterion, criterion_group, criterion_main};
 use sandsim::component::{grid::Grid, particles::particle::Particle};
 
+fn fill_grid_mixed(g: &mut Grid, (x, y): (usize, usize)) {
+    for y in 0..y / 3 {
+        for x in 0..x {
+            g.spawn_particle(x, y, Particle::Sand);
+        }
+    }
+    for y in y / 3..y {
+        for x in 0..x {
+            g.spawn_particle(x, y, Particle::Water);
+        }
+    }
+    for y in 2 * y / 3..y {
+        for x in 0..x {
+            g.spawn_particle(x, y, Particle::Salt);
+        }
+    }
+}
+
 fn criterion_benchmark(c: &mut Criterion) {
+    c.bench_function("update grid sand", |b| {
+        let y = 1920;
+        let x = 1080;
+        let mut g = Grid::new(x, y);
+        for y in 0..y {
+            for x in 0..x {
+                g.spawn_particle(x, y, Particle::Sand);
+            }
+        }
+        b.iter(|| {
+            g.update_grid();
+        });
+    });
+
+    c.bench_function("update grid water", |b| {
+        let y = 1920;
+        let x = 1080;
+        let mut g = Grid::new(x, y);
+        for y in 0..y {
+            for x in 0..x {
+                g.spawn_particle(x, y, Particle::Water);
+            }
+        }
+        b.iter(|| {
+            g.update_grid();
+        });
+    });
+
+    c.bench_function("update grid salt", |b| {
+        let y = 1920;
+        let x = 1080;
+        let mut g = Grid::new(x, y);
+        for y in 0..y {
+            for x in 0..x {
+                g.spawn_particle(x, y, Particle::Salt);
+            }
+        }
+        b.iter(|| {
+            g.update_grid();
+        });
+    });
+
     c.bench_function("update grid", |b| {
         let y = 1920;
         let x = 1080;
         let mut g = Grid::new(x, y);
-        (0..x)
-            .zip(0..y / 2)
-            .for_each(|(x, y)| g.spawn_particle(x, y, Particle::Sand));
-        (0..x)
-            .zip(y / 2..y)
-            .for_each(|(x, y)| g.spawn_particle(x, y, Particle::Water));
+        fill_grid_mixed(&mut g, (x, y));
         b.iter(|| {
             g.update_grid();
         });
@@ -22,12 +77,7 @@ fn criterion_benchmark(c: &mut Criterion) {
         let x = 1080;
         let mut g = Grid::new(x, y);
         let mut image = Grid::create_output_frame(x, y);
-        (0..x)
-            .zip(0..y / 2)
-            .for_each(|(x, y)| g.spawn_particle(x, y, Particle::Sand));
-        (0..x)
-            .zip(y / 2..y)
-            .for_each(|(x, y)| g.spawn_particle(x, y, Particle::Water));
+        fill_grid_mixed(&mut g, (x, y));
         b.iter(|| {
             g.draw_grid(&mut image);
         });
