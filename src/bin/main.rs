13b6b3e0b9f30857b84c6a95dc2e6ece5168a860
mksafavi <mksafavi@gmail.com>
2025-10-07 16:56:08 +0330

    remove mouse only inputs system

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,5 +1,5 @@
 use bevy::DefaultPlugins;
-use bevy::app::{App, FixedUpdate, Startup};
+use bevy::app::{App, Startup};
 use bevy::camera::Camera2d;
 use bevy::ecs::system::Commands;
 use bevy::image::ImagePlugin;
@@ -7,7 +7,6 @@ use bevy::prelude::PluginGroup;
 use bevy::utils::default;
 use bevy::window::{Window, WindowPlugin, WindowResolution};
 use sandsim::component::grid_plugin::{ConfigResource, GridPlugin};
-use sandsim::component::inputs;
 
 fn setup_camera(mut commands: Commands) {
     commands.spawn(Camera2d);
@@ -31,6 +30,5 @@ fn main() {
             config: ConfigResource::new(300, 300, 240.),
         })
         .add_systems(Startup, setup_camera)
-        .add_systems(FixedUpdate, inputs::mouse_spawn_brush_system)
         .run();
 }
--- src/component.rs
+++ src/component.rs
@@ -1,5 +1,4 @@
 pub mod grid;
 pub mod grid_plugin;
-pub mod inputs;
 pub mod macros;
 pub mod particles;
