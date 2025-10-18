f67f08185e14a2c9587813ef114f49483fec9379
mksafavi <mksafavi@gmail.com>
2025-10-18 15:29:41 +0330

    render UiPass before post processing

--- src/component/render.rs
+++ src/component/render.rs
@@ -1,11 +1,29 @@
 use bevy::{
     app::{App, Plugin, Startup},
     camera::Camera2d,
-    ecs::system::Commands,
+    core_pipeline::core_2d::graph::{Core2d, Node2d},
+    ecs::{system::Commands, world::Mut},
+    render::{RenderApp, render_graph::RenderGraph},
+    ui_render::graph::NodeUi,
 };
 
 fn setup_camera(mut commands: Commands) {
     commands.spawn(Camera2d);
+}
+
+pub fn move_ui_pass_to_before_postprocessing_end_node(app: &mut App) {
+    if let Some(render_app) = app.get_sub_app_mut(RenderApp) {
+        render_app
+            .world_mut()
+            .resource_scope(|_, mut graph: Mut<RenderGraph>| {
+                if let Some(graph_2d) = graph.get_sub_graph_mut(Core2d) {
+                    let _ = graph_2d
+                        .remove_node_edge(Node2d::EndMainPassPostProcessing, NodeUi::UiPass);
+                }
+            });
+    } else {
+        panic!("no render app");
+    }
 }
 
 pub struct RenderSimPlugin;
@@ -13,5 +31,6 @@ pub struct RenderSimPlugin;
 impl Plugin for RenderSimPlugin {
     fn build(&self, app: &mut App) {
         app.add_systems(Startup, setup_camera);
+        move_ui_pass_to_before_postprocessing_end_node(app);
     }
 }
